96e4628ceba75ab66f9f2c5ea9b1044a1f9a4ec6
Alan Brockett <abrockett@gmail.com>
2013-06-11 00:03:32 -0600

    lumenize version - nonworking

--- App.js
+++ App.js
@@ -2,76 +2,78 @@ Ext.define('DefectGridApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
 
-    items: [
-        {
-            xtype: 'rallychart',
-
-            storeConfig: {
-                fetch: ['Name','Environment','OpenedDate','Project','CreationDate'],
-                hydrate: ['Environment'],
-                filters: [
-                    {
-                        property: '_ProjectHierarchy',
-                        operator: '=',
-                        value: 1971104447
-                    },
-                    {
-                        property: '_TypeHierarchy',
-                        operator: '=',
-                        value: 'Defect'
-                    },
-                    {
-                        property: 'Resolution',
-                        operator: '=',
-                        value: 'Duplicate'
-                    },
-                    {
-                        property: '_ValidFrom',
-                        operator: '=',
-                        value: {
-                            $gte: "2010-02-01TZ",
-                            $lte: "current"
-                        }
-                    }
-                ]
-            },
-
-            calculatorType: 'ProdVsTestCalculator',
-            calculatorConfig: {
-            },
-
-            chartConfig: {
-                chart: {
-                    zoomType: 'xy'
-                },
-                title: {
-                    text: 'Defect Trends'
-                },
-                xAxis: {
-                    tickmarkPlacement: 'on',
-                    tickInterval: 2,
-                    title: {
-                        text: 'Month'
-                    }
-                },
-                yAxis: [
-                    {
-                        title: {
-                            text: 'Count'
-                        }
-                    }
-                ],
-                plotOptions: {
-                    series: {
-                        marker: {
-                            enabled: false
-                        }
-                    },
-                    area: {
-                        stacking: 'normal'
-                    }
+            
+    launch: function() {
+        var dataScope = this.getContext().getDataContext();
+        var chart = Ext.create('Rally.ui.chart.Chart', {});
+        Ext.create('Rally.data.WsapiDataStore', {
+            model: 'Defect',
+            context: dataScope,
+            fetch: ['Name', 'CreationDate'],
+            sorters: [
+                {
+                    property: 'CreationDate',
+                    direction: 'ASC'
                 }
+            ],
+            pageSize: 5,
+            limit: 5,
+            autoLoad: true,
+            listeners: {
+                load: this._onDataLoaded,
+                scope: this
             }
-        }
-    ]
+        });
+    },
+
+    _onDataLoaded: function(store, data) {
+        debugger;
+        var snapshots = [];
+        var dateTime;
+        Ext.Array.each(data, function(record) {
+            //Perform custom actions with the data here
+
+            dateTime = Rally.data.lookback.Lumenize.Time.getISOStringFromJSDate(record.get('CreationDate'))
+            snapshots.push({
+                Name: record.get('Name'),
+                '_ValidFrom': dateTime,
+                '_ValidTo': dateTime,
+                CreationDate: record.get('CreationDate')
+            });
+        });
+
+        var metrics = [
+            {as: 'DefectCount', f: 'count'}
+        ]
+
+        var config = {
+            metrics: metrics,
+            granularity: Rally.data.lookback.Lumenize.Time.MONTH,
+            tz: 'America/Denver'
+        };
+
+        var calculator = new Rally.data.lookback.Lumenize.TimeSeriesCalculator(config);
+
+        var startOnISOString = new Rally.data.lookback.Lumenize.Time('2010-01-01').getISOStringInTZ(config.tz);
+        var upToDateISOString = new Rally.data.lookback.Lumenize.Time('2013-06-10').getISOStringInTZ(config.tz);
+        calculator.addSnapshots(snapshots, startOnISOString, upToDateISOString);
+
+        console.log(calculator.getResults());
+
+        // this.add({
+        //     xtype: 'rallygrid',
+        //     store: Ext.create('Rally.data.custom.Store', {
+        //         data: records,
+        //         pageSize: 200
+        //     }),
+        //     columnCfgs: [
+        //         {
+        //             text: 'Name', dataIndex: 'Name', flex: 1
+        //         },
+        //         {
+        //             text: 'Created', dataIndex: 'CreationDate', width: 300
+        //         }
+        //     ]
+        // });
+    }
 });
