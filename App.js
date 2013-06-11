0a765704edf8d75473a40df65342afccdf65a28a
Alan Brockett <abrockett@gmail.com>
2013-06-11 00:17:37 -0600

    Converted to use lo-dash

--- App.js
+++ App.js
@@ -28,37 +28,20 @@ Ext.define('DefectGridApp', {
 
     _onDataLoaded: function(store, data) {
         debugger;
-        var snapshots = [];
-        var dateTime;
+        var records = [];
+        var date;
         Ext.Array.each(data, function(record) {
             //Perform custom actions with the data here
 
-            dateTime = Rally.data.lookback.Lumenize.Time.getISOStringFromJSDate(record.get('CreationDate'))
-            snapshots.push({
+            date = record.get('CreationDate').getFullYear() + ' ' + record.get('CreationDate').getMonth();
+            records.push({
                 Name: record.get('Name'),
-                '_ValidFrom': dateTime,
-                '_ValidTo': dateTime,
-                CreationDate: record.get('CreationDate')
+                CreationDate: date
             });
         });
-
-        var metrics = [
-            {as: 'DefectCount', f: 'count'}
-        ]
-
-        var config = {
-            metrics: metrics,
-            granularity: Rally.data.lookback.Lumenize.Time.MONTH,
-            tz: 'America/Denver'
-        };
-
-        var calculator = new Rally.data.lookback.Lumenize.TimeSeriesCalculator(config);
-
-        var startOnISOString = new Rally.data.lookback.Lumenize.Time('2010-01-01').getISOStringInTZ(config.tz);
-        var upToDateISOString = new Rally.data.lookback.Lumenize.Time('2013-06-10').getISOStringInTZ(config.tz);
-        calculator.addSnapshots(snapshots, startOnISOString, upToDateISOString);
-
-        console.log(calculator.getResults());
+        console.log(records);
+        series = _.groupBy(records, function(record) { return record.CreationDate });
+        console.log(series);
 
         // this.add({
         //     xtype: 'rallygrid',
